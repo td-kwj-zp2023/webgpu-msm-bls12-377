718a9a054d6aca572b6466f523888e5995e70466
Tal Derei <talderei99@gmail.com>
2023-10-17 08:38:03 -0700

    added UI components

--- src/submission/submission.ts
+++ src/submission/submission.ts
@@ -1,11 +1,15 @@
 import { BigIntPoint, U32ArrayPoint } from "../reference/types";
-import { execute_cuzk } from "./cuzk/cuzk"
+import { execute_cuzk } from "./cuzk/cuzk_serial"
+import { execute_cuzk_parallel } from "./cuzk/cuzk_parallel"
+import { execute_cuzk_wgsl } from "./cuzk/cuzk_wgsl"
 
 // Typescript implementation of cuZK
 export const compute_cuzk_typescript = async (
   baseAffinePoints: BigIntPoint[],
   scalars: bigint[]
 ): Promise<{x: bigint, y: bigint}> => {
+  console.log("Starting Serial cuZK!")
+
   const inputSize = 16
 
   const result = await execute_cuzk(inputSize, baseAffinePoints, scalars)
@@ -17,10 +21,34 @@ export const compute_cuzk_typescript = async (
   return { x, y }
 };
 
+// Typescript implementation of cuZK with web-workers
+export const compute_cuzk_typescript_web_workers = async (
+  baseAffinePoints: BigIntPoint[],
+  scalars: bigint[]
+): Promise<any> => {
+  console.log("Starting Parallel cuZK!")
+
+  const inputSize = 16
+
+  const result = await execute_cuzk_parallel(inputSize, baseAffinePoints, scalars)
+
+  // const result_affine = result.toAffine()
+  // const x = result_affine.x
+  // const y = result_affine.y
+
+  // return { x, y }
+};
+
 // WGSL implementation of cuZK
 export const compute_cuzk_wgsl = async (
-  baseAffinePoints: BigIntPoint[] | U32ArrayPoint[],
-  scalars: bigint[] | Uint32Array[]
+  baseAffinePoints: BigIntPoint[],
+  scalars: bigint[]
 ): Promise<{x: bigint, y: bigint}> => {
+  console.log("Starting WGSL cuZK!")
+  
+  const inputSize = 16
+
+  const result = await execute_cuzk_wgsl(inputSize, baseAffinePoints, scalars)
+
   throw new Error("Not implemented");
 };
