480c9cf792ff399e88a353e73e8ee092d3165dfd
Koh Wei Jie <contact@kohweijie.com>
2023-11-04 22:38:57 -0700

    multiple GPU invocations per subtask based on the cluster-start-indices size

--- src/submission/create_ell_from_points_gpu.ts
+++ src/submission/create_ell_from_points_gpu.ts
@@ -8,6 +8,7 @@ import {
     u8s_to_points,
     points_to_u8s_for_gpu,
     numbers_to_u8s_for_gpu,
+    u8s_to_numbers,
 } from './utils'
 import {
     prep_for_cluster_method,
@@ -110,6 +111,10 @@ export async function create_ell_gpu(
     num_rows: number,
     params: any,
 ) {
+    const num_words = params.num_words
+    const n0 = params.n0
+    const rinv = params.rinv
+
     const data: ExtPointType[][] = []
     const col_idx: number[][] = []
     const row_length: number[] = []
@@ -154,9 +159,6 @@ export async function create_ell_gpu(
     const all_new_point_indices = []
     const all_cluster_start_indices = []
 
-    // TODO: handle cluster_start_indices for points what don't need to be
-    // added to any other points!
-
     const expected_new_points = []
     const expected_scalar_chunks = []
 
@@ -198,52 +200,26 @@ export async function create_ell_gpu(
         }
     }
 
-    // Since we set workgroup_size to 256, each x workgroup can operate on 256 values at a time.
-
-    const num_x_workgroups = 1
-
-    const num_words = params.num_words
-    //const r = params.r
-    const n0 = params.n0
-    const rinv = params.rinv
-
-    const start = Date.now()
-
     // Convert inputs to bytes
     const scalar_chunks_bytes = numbers_to_u8s_for_gpu(scalar_chunks)
     const all_new_point_indices_bytes = numbers_to_u8s_for_gpu(all_new_point_indices)
     const all_cluster_start_indices_bytes = numbers_to_u8s_for_gpu(all_cluster_start_indices)
 
+    // Each x workgroup can operate on workgroup_size values at a time.
+    const workgroup_size = 256
+    const max_threads = workgroup_size * 256
+    const num_new_points = all_cluster_start_indices.length
+
+    let num_invocations = Math.floor(num_new_points / max_threads)
+    if (num_new_points % max_threads > 0) {
+        num_invocations += 1
+    }
+
     const scalar_chunks_storage_buffer = device.createBuffer({
         size: scalar_chunks_bytes.length,
         usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
     });
-    const all_new_point_indices_storage_buffer = device.createBuffer({
-        size: all_new_point_indices_bytes.length,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
-    });
-    const all_cluster_start_indices_storage_buffer = device.createBuffer({
-        size: all_cluster_start_indices_bytes.length,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
-    });
-
-    // This takes up a lot of time - can it be done in the GPU instead?
     device.queue.writeBuffer(scalar_chunks_storage_buffer, 0, scalar_chunks_bytes);
-    device.queue.writeBuffer(all_new_point_indices_storage_buffer, 0, all_new_point_indices_bytes);
-    device.queue.writeBuffer(all_cluster_start_indices_storage_buffer, 0, all_cluster_start_indices_bytes);
-
-    // Output buffers
-    const new_points_storage_buffer = device.createBuffer({
-        //size: points_bytes.length,
-        size: all_cluster_start_indices.length * 320, //(points_bytes.length / points.length),
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
-    });
-
-    const new_scalar_chunks_storage_buffer = device.createBuffer({
-        //size: scalar_chunks_bytes.length,
-        size: all_cluster_start_indices.length * 4,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
-    });
 
     const bindGroupLayout = device.createBindGroupLayout({
         entries: [
@@ -254,20 +230,7 @@ export async function create_ell_gpu(
             { binding: 4, visibility: GPUShaderStage.COMPUTE, buffer: { type: "storage" } },
             { binding: 5, visibility: GPUShaderStage.COMPUTE, buffer: { type: "storage" } },
         ]
-    });
-
-    const bindGroup = create_bind_group(
-        device, 
-        bindGroupLayout,
-        [
-            points_storage_buffer,
-            scalar_chunks_storage_buffer,
-            all_new_point_indices_storage_buffer,
-            all_cluster_start_indices_storage_buffer,
-            new_points_storage_buffer,
-            new_scalar_chunks_storage_buffer,
-        ],
-    )
+    })
 
     const p_limbs = gen_p_limbs(p, num_words, word_size)
     const shaderCode = mustache.render(
@@ -303,78 +266,158 @@ export async function create_ell_gpu(
         }
     });
 
-    const commandEncoder = device.createCommandEncoder()
-    const passEncoder = commandEncoder.beginComputePass()
-    passEncoder.setPipeline(computePipeline)
-    passEncoder.setBindGroup(0, bindGroup)
-    passEncoder.dispatchWorkgroups(num_x_workgroups)
-    passEncoder.end()
+    const do_gpu_invocation = async (
+        num_x_workgroups: number,
+        invocation_new_point_indices_bytes: Uint8Array,
+        invocation_cluster_start_indices_bytes: Uint8Array,
+    ) => {
+        const start = Date.now()
+        const invocation_new_point_indices_storage_buffer = device.createBuffer({
+            size: invocation_new_point_indices_bytes.length,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
+        })
 
-    const new_points_staging_buffer = device.createBuffer({
-        size: new_points_storage_buffer.size,
-        usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
-    });
-    const new_scalar_chunks_staging_buffer = device.createBuffer({
-        size: new_scalar_chunks_storage_buffer.size,
-        usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
-    });
+        const invocation_cluster_start_indices_storage_buffer = device.createBuffer({
+            size: invocation_cluster_start_indices_bytes.length,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
+        })
 
-    commandEncoder.copyBufferToBuffer(
-        new_points_storage_buffer,
-        0,
-        new_points_staging_buffer,
-        0,
-        new_points_storage_buffer.size
-    );
-    commandEncoder.copyBufferToBuffer(
-        new_scalar_chunks_storage_buffer,
-        0,
-        new_scalar_chunks_staging_buffer,
-        0,
-        new_scalar_chunks_storage_buffer.size
-    );
+        device.queue.writeBuffer(invocation_new_point_indices_storage_buffer, 0, invocation_new_point_indices_bytes);
+        device.queue.writeBuffer(invocation_cluster_start_indices_storage_buffer, 0, invocation_cluster_start_indices_bytes);
 
-    device.queue.submit([commandEncoder.finish()]);
+        const num_new_points = invocation_new_point_indices_bytes.length / 4
 
-    // map staging buffers to read results back to JS
-    await new_points_staging_buffer.mapAsync(
-        GPUMapMode.READ,
-        0,
-        new_points_storage_buffer.size
-    );
-    await new_scalar_chunks_staging_buffer.mapAsync(
-        GPUMapMode.READ,
-        0,
-        new_scalar_chunks_storage_buffer.size
-    );
+        // Output buffers
+        const new_points_storage_buffer = device.createBuffer({
+            size: num_new_points * 320,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
+        })
 
-    const np = new_points_staging_buffer.getMappedRange(0, new_points_staging_buffer.size)
-    const new_points_data = np.slice(0)
-    new_points_staging_buffer.unmap()
+        const new_scalar_chunks_storage_buffer = device.createBuffer({
+            size: invocation_cluster_start_indices_bytes.length,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
+        })
 
-    const ns = new_scalar_chunks_staging_buffer.getMappedRange(0, new_scalar_chunks_staging_buffer.size)
-    const new_scalar_chunks_data = ns.slice(0)
-    new_scalar_chunks_staging_buffer.unmap()
+        const bindGroup = create_bind_group(
+            device, 
+            bindGroupLayout,
+            [
+                points_storage_buffer,
+                scalar_chunks_storage_buffer,
+                invocation_new_point_indices_storage_buffer,
+                invocation_cluster_start_indices_storage_buffer,
+                new_points_storage_buffer,
+                new_scalar_chunks_storage_buffer,
+            ],
+        )
 
-    const elapsed = Date.now() - start
-    console.log(`GPU took ${elapsed}ms`)
+        const commandEncoder = device.createCommandEncoder()
+        const passEncoder = commandEncoder.beginComputePass()
+        passEncoder.setPipeline(computePipeline)
+        passEncoder.setBindGroup(0, bindGroup)
+        passEncoder.dispatchWorkgroups(num_x_workgroups)
+        passEncoder.end()
 
-    const new_points = u8s_to_points(new Uint8Array(new_points_data), num_words, word_size)
+        const new_points_staging_buffer = device.createBuffer({
+            size: new_points_storage_buffer.size,
+            usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
+        });
+        const new_scalar_chunks_staging_buffer = device.createBuffer({
+            size: new_scalar_chunks_storage_buffer.size,
+            usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
+        });
 
-    // convert output_points out of Montgomery coords
-    const new_points_non_mont: ExtPointType[] = []
+        commandEncoder.copyBufferToBuffer(
+            new_points_storage_buffer,
+            0,
+            new_points_staging_buffer,
+            0,
+            new_points_storage_buffer.size
+        );
+        commandEncoder.copyBufferToBuffer(
+            new_scalar_chunks_storage_buffer,
+            0,
+            new_scalar_chunks_staging_buffer,
+            0,
+            new_scalar_chunks_storage_buffer.size
+        );
 
-    // Convert out of Mont form
-    for (const pt of new_points) {
-        const non = {
-            x: fieldMath.Fp.mul(pt.x, rinv),
-            y: fieldMath.Fp.mul(pt.y, rinv),
-            t: fieldMath.Fp.mul(pt.t, rinv),
-            z: fieldMath.Fp.mul(pt.z, rinv),
+        device.queue.submit([commandEncoder.finish()]);
+
+        // map staging buffers to read results back to JS
+        await new_points_staging_buffer.mapAsync(
+            GPUMapMode.READ,
+            0,
+            new_points_storage_buffer.size
+        );
+        await new_scalar_chunks_staging_buffer.mapAsync(
+            GPUMapMode.READ,
+            0,
+            new_scalar_chunks_storage_buffer.size
+        );
+
+        const np = new_points_staging_buffer.getMappedRange(0, new_points_staging_buffer.size)
+        const new_points_data = np.slice(0)
+        new_points_staging_buffer.unmap()
+
+        const ns = new_scalar_chunks_staging_buffer.getMappedRange(0, new_scalar_chunks_staging_buffer.size)
+        const new_scalar_chunks_data = ns.slice(0)
+        new_scalar_chunks_staging_buffer.unmap()
+
+        const new_scalar_chunks = u8s_to_numbers(new Uint8Array(new_scalar_chunks_data))
+        const new_points = u8s_to_points(new Uint8Array(new_points_data), num_words, word_size)
+
+        const elapsed = Date.now() - start
+        console.log(`GPU took ${elapsed}ms`)
+        console.log('Note that converting bytes to BigInts and ExtPointTypes is inefficient')
+
+        // Convert out of Mont form
+        const new_points_non_mont: ExtPointType[] = []
+        for (const pt of new_points) {
+            const non = {
+                x: fieldMath.Fp.mul(pt.x, rinv),
+                y: fieldMath.Fp.mul(pt.y, rinv),
+                t: fieldMath.Fp.mul(pt.t, rinv),
+                z: fieldMath.Fp.mul(pt.z, rinv),
+            }
+            new_points_non_mont.push(bigIntPointToExtPointType(non, fieldMath))
         }
-        new_points_non_mont.push(bigIntPointToExtPointType(non, fieldMath))
+
+        return {
+            new_points_non_mont,
+            new_scalar_chunks,
+        }
     }
-    debugger
+
+    for (let invocation_idx = 0; invocation_idx < num_invocations; invocation_idx ++) {
+        const num_points_for_this_invocation =
+            invocation_idx === num_invocations - 1 ? 
+                num_new_points % max_threads
+                :
+                max_threads
+
+        const num_x_workgroups = num_points_for_this_invocation % 256 === 0 ?
+            num_points_for_this_invocation / 256
+            :
+            Math.floor(num_points_for_this_invocation / 256) + 1
+
+        const invocation_new_point_indices_bytes = all_new_point_indices_bytes.slice(
+            invocation_idx * max_threads * 4,
+            (invocation_idx * max_threads + num_points_for_this_invocation) * 4,
+        )
+        const invocation_cluster_start_indices_bytes = all_cluster_start_indices_bytes.slice(
+            invocation_idx * max_threads,
+            (invocation_idx * max_threads + num_points_for_this_invocation) * 4,
+        )
+
+        console.log(`Invocation ${invocation_idx}`)
+        const { new_points_non_mont, new_scalar_chunks } =
+            await do_gpu_invocation(
+                num_x_workgroups,
+                invocation_new_point_indices_bytes,
+                invocation_cluster_start_indices_bytes,
+            )
+    }
 
     return new ELLSparseMatrix(data, col_idx, row_length)
 }
