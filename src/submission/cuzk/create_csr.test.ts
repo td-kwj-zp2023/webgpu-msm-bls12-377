a285514f4e1b54fbbd740ec9fe6df0b48c9d54c3
Koh Wei Jie <contact@kohweijie.com>
2023-11-08 19:35:48 -0800

    wrote pre-aggregation methods (cluster method) in Typescript; added 1 small test

---
+++ src/submission/cuzk/create_csr.test.ts
@@ -0,0 +1,54 @@
+import { create_csr } from './create_csr'
+
+describe('Create an CSR sparse matrix from the MSM input points and scalars', () => {
+    describe('pre-aggregation using the cluster method', () => {
+        describe('small tests', () => {
+            const num_points = 8
+            const num_rows = 2
+            it('small test #1', () => {
+
+                const points = []
+                for (let i = 0; i < num_points; i ++) {
+                    points.push(`P${i}`)
+                }
+
+                const decomposed_scalars = [
+                    [
+                        4, 4, 4, 3,
+                        3, 3, 3, 0,
+                    ],
+                    [
+                        3, 4, 4, 3,
+                        4, 1, 0, 2,
+                    ],
+                ]
+
+                const expected_data = [
+                    ['P2P1P0', 'P3', 'P6P5P4'],
+                    ['P2P1', 'P3P0', 'P4', 'P5', 'P7'],
+                ]
+
+                const expected_col_idxs = [
+                    [4, 3, 3],
+                    [4, 3, 4, 1, 2],
+                ]
+
+                const expected_row_ptrs = [
+                    [0, 3, 4, 7],
+                    [0, 2, 4, 5, 6, 7],
+                ]
+
+                for (let i = 0; i < decomposed_scalars.length; i ++) {
+                    const scalar_chunks = decomposed_scalars[i]
+                    const csr_sm = create_csr(points, scalar_chunks, num_rows)
+                    expect(csr_sm.data.toString()).toEqual(expected_data[i].toString())
+                    expect(csr_sm.col_idx.toString()).toEqual(expected_col_idxs[i].toString())
+                    expect(csr_sm.row_ptr.toString()).toEqual(expected_row_ptrs[i].toString())
+                }
+
+            })
+        })
+    })
+})
+
+export {}
