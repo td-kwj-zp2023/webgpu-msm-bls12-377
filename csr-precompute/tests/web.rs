5ca1797b19aa92ce537d1538bb1ddb8f1f96cf53
Koh Wei Jie <contact@kohweijie.com>
2023-11-13 15:32:26 +0800

    CSR precomputation in WASM

--- csr-precompute/tests/web.rs
+++ csr-precompute/tests/web.rs
@@ -4,12 +4,23 @@
 
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
-use csr_precompute::precompute_with_cluster_method;
+use csr_precompute::{
+    all_precomputation,
+    AllPrecomputeResult,
+};
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
-    precompute_with_cluster_method();
-    assert_eq!(1 + 1, 2);
+    let num_rows = 2;
+    let scalar_chunks = vec![4, 4, 4, 3, 3, 3, 3, 0];
+    let result: AllPrecomputeResult = serde_wasm_bindgen::from_value(all_precomputation(scalar_chunks.as_slice(), num_rows)).unwrap();
+    println!("{}", result.all_new_point_indices.len());
+    println!("{:?}", result.all_new_point_indices);
+    println!("{:?}", result.all_cluster_start_indices);
+    println!("{:?}", result.all_cluster_end_indices);
+    println!("{:?}", result.all_single_point_indices);
+    println!("{:?}", result.all_single_scalar_chunks);
+    println!("{:?}", result.row_ptr);
 }
