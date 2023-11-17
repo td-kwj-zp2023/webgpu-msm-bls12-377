d297ff0495c7f4f5c0437a90582b14e0a259f8b6
Koh Wei Jie <contact@kohweijie.com>
2023-11-18 07:12:51 +0800

    gpu benchmarks for scalar decomp for 13-bit integers

--- src/submission/decompose_scalars_benchmark.ts
+++ src/submission/decompose_scalars_benchmark.ts
@@ -8,6 +8,7 @@ import {
     u8s_to_numbers,
     decompose_scalars,
     compute_misc_params,
+    bigints_to_u8_for_gpu,
 } from './utils'
 import { get_device, create_bind_group } from './gpu'
 import decompose_scalars_shader from './wgsl/decompose_scalars.template.wgsl'
@@ -20,7 +21,6 @@ export const decompose_scalars_ts_benchmark = async (
     const num_words = 20
     const word_size = 13
 
-    /*
     console.log('Typescript benchmarks:')
     //for (let word_size = 13; word_size < 14; word_size ++) {
     for (let word_size = 8; word_size < 20; word_size ++) {
@@ -51,15 +51,85 @@ export const decompose_scalars_ts_benchmark = async (
     assert(ts_r.toString() === wasm_r.toString())
     console.log('ok')
 
-    //debugger
-    */
-
-    console.log('GPU benchmarks:')
-    const ts_r = decompose_scalars(scalars, num_words, word_size).flat()
-    console.log(ts_r)
+    console.log('GPU benchmarks (13-bit windows):')
     await decompose_scalars_gpu(scalars, 20, 13)
 
     return { x: BigInt(0), y: BigInt(0) }
+}
+
+const extract_word_from_bytes_le = (
+    input: number[],
+    word_idx: number,
+    word_size: number,
+): number => {
+    /*
+     * 
+     * ---- ---- ---- ---- .... .... .... ....
+     * ++++ ++xx xxxx xxxx xxx+ ++++ ++++ ++++
+     *
+     * ---- ---- ---- ---- .... .... .... ....
+     * ++++ ++++ ++++ xxxx xxxx xxxx x+++ ++++
+     *
+     * ---- ---- ---- ---- .... .... .... ....
+     * xxxx x+++ ++++ ++++ ++xx xxxx xxxx xxx+
+     *
+     * 15, 13, 15, 0
+     * 14, 10, 15, 13
+     * 13,  7, 14, 10
+     * 12,  4, 13,  7
+     * 11,  1, 12,  4
+     * 11, 14, 11,  1
+     */
+    const start_byte_idx = 15 - Math.floor((word_idx * word_size + word_size) / 16)
+    const end_byte_idx = 15 - Math.floor((word_idx * word_size) / 16)
+
+    const start_byte_offset = (word_idx * word_size + word_size) % 16
+    const end_byte_offset = (word_idx * word_size) % 16
+
+    const mask = 2 ** start_byte_offset - 1
+    let word = 0
+
+    if (start_byte_idx === end_byte_idx) {
+        word = (input[start_byte_idx] & mask) >> end_byte_offset
+    } else {
+        word = (input[start_byte_idx] & mask) << (16 - end_byte_offset) 
+        word += input[end_byte_idx] >> end_byte_offset
+    }
+
+    return word
+}
+
+const bigint_to_words_32 = (val: bigint) => {
+    let hex_str = val.toString(16)
+    while (hex_str.length < 64) {
+        hex_str = '0' + hex_str
+    }
+    const double_bytes: number[] = []
+    for (let i = 0; i < hex_str.length; i += 4) {
+        const db = Number('0x' + hex_str.slice(i, i + 4))
+        double_bytes.push(db)
+    }
+    return double_bytes
+}
+
+const to_words_le_modified = (
+    val: bigint,
+    num_words: number,
+    word_size: number,
+): Uint16Array => {
+    const double_bytes = bigint_to_words_32(val)
+
+    const words = Array(num_words).fill(0)
+    for (let i = 0; i < num_words - 1; i ++) {
+        const w = extract_word_from_bytes_le(double_bytes, i, word_size)
+        words[i] = w
+    }
+    
+    //      ---- ---- ---- ---- last 16 bits
+    // .... .... .... .... .... last 20 bits
+    // xxxx xxxx xxxx x--- ---- last 13 bits
+    words[num_words - 1] = double_bytes[0] >> 7
+    return new Uint16Array(words)
 }
 
 const decompose_scalars_gpu = async (
@@ -67,17 +137,20 @@ const decompose_scalars_gpu = async (
     num_words: number,
     word_size: number,
 ) => {
-    //console.log(scalars)
     // Convert scalars to bytes
-    const scalar_bytes = new Uint8Array(scalars.length * 32)
+    const scalar_bytes = bigints_to_u8_for_gpu(scalars, 16, 16)
+
+    // Calculate expected decomposed scalars
+    const expected: number[][] = []
+    const expected2: number[][] = []
     for (let i = 0; i < scalars.length; i ++) {
-        const scalar = scalars[i]
-        const buf = toBufferBE(scalar, 32)
-        scalar_bytes.set(buf, i * 32)
+        expected.push(Array.from(to_words_le_modified(scalars[i], num_words, word_size)))
+        expected2.push(Array.from(to_words_le(scalars[i], num_words, word_size)))
     }
+    assert(expected.toString() === expected2.toString())
 
     const device = await get_device()
-    const num_x_workgroups = 2
+    const num_x_workgroups = 256
 
     const scalars_storage_buffer = device.createBuffer({
         size: scalar_bytes.length,
@@ -87,7 +160,7 @@ const decompose_scalars_gpu = async (
 
     // Output buffers
     const result_storage_buffer = device.createBuffer({
-        size: num_words * scalars.length * 8,
+        size: num_words * scalars.length * 4,
         usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
     })
 
@@ -126,6 +199,8 @@ const decompose_scalars_gpu = async (
         }
     })
 
+    const start = Date.now();
+
     const commandEncoder = device.createCommandEncoder()
     const passEncoder = commandEncoder.beginComputePass()
     passEncoder.setPipeline(computePipeline)
@@ -153,9 +228,23 @@ const decompose_scalars_gpu = async (
         result_storage_buffer.size
     )
 
+    const elapsed = Date.now() - start
+    console.log(`GPU took ${elapsed}ms`)
+
     const result_data = result_staging_buffer.getMappedRange(0, result_staging_buffer.size).slice(0)
     result_staging_buffer.unmap()
 
     const scalar_chunks = u8s_to_numbers(new Uint8Array(result_data))
-    console.log(scalar_chunks)
+    //console.log('from gpu:', scalar_chunks)
+    for (let i = 0; i < expected.length; i ++) {
+        for (let j = 0; j < num_words; j ++) {
+            const e = expected[i][j]
+            const s = scalar_chunks[i * num_words + j]
+            if (e !== s) {
+                console.log(i, j)
+                debugger
+            }
+            assert(e === s)
+        }
+    }
 }
