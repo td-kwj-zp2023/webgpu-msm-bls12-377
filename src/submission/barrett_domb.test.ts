d5e11951720996f09806423839276d3a5e84fc05
Koh Wei Jie <contact@kohweijie.com>
2023-11-24 17:51:05 +0800

    strenghtened tests

--- src/submission/barrett_domb.test.ts
+++ src/submission/barrett_domb.test.ts
@@ -1,5 +1,6 @@
 import {
     to_words_le,
+    from_words_le,
     genRandomFieldElement,
 } from './utils'
 import {
@@ -49,6 +50,19 @@ describe('Barrett-Domb ', () => {
             [18063, 48642, 13689, 44273, 9320, 39904, 4951, 35535, 582, 31166, 61749, 26796, 57380, 22427, 53011, 1162].toString())
     })
 
+    it('mp_msb_multiply 2', () => {
+        const num_words = 20
+        const word_size = 13
+        const a = BigInt('12606796758224846727326035948803889824738128609730484894316100840265045196027')
+        const a_words = to_words_le(a, num_words, word_size)
+        const b = BigInt('14276552610056165753848820256553331055663673083569154091093918058913504134283')
+        const b_words = to_words_le(b, num_words, word_size)
+
+        const result = mp_msb_multiply(a_words, b_words, num_words, word_size)
+        expect(result.toString()).toEqual(
+            [2165, 4700, 172, 2275, 6372, 2494, 3559, 3643, 4358, 3285, 3136, 6280, 1860, 5007, 6542, 954, 409, 7593, 3518, 0].toString())
+    })
+
     it('mp_lsb_multiply', () => {
         const num_words = 16
         const word_size = 16
@@ -60,6 +74,19 @@ describe('Barrett-Domb ', () => {
         const result = mp_lsb_multiply(a_words, b_words, num_words, word_size, true)
         expect(result.toString()).toEqual(
             [3782, 38739, 8155, 43108, 12524, 47477, 16893, 51846, 21262, 56215, 25631, 60584, 30000, 64953, 34369, 20682, 9905].toString())
+    })
+
+    it('mp_lsb_multiply 2', () => {
+        const num_words = 20
+        const word_size = 13
+        const a = BigInt('12606796758224846727326035948803889824738128609730484894316100840265045196027')
+        const a_words = to_words_le(a, num_words, word_size)
+        const b = BigInt('14276552610056165753848820256553331055663673083569154091093918058913504134283')
+        const b_words = to_words_le(b, num_words, word_size)
+
+        const result = mp_lsb_multiply(a_words, b_words, num_words, word_size, true)
+        expect(result.toString()).toEqual(
+            [2121, 7108, 8052, 630, 3743, 7907, 2443, 7965, 3945, 757, 6249, 4778, 5546, 4338, 4253, 7207, 1664, 1300, 4815, 6771, 1483].toString())
     })
 
     it('mp_adder', () => {
@@ -110,40 +137,52 @@ describe('Barrett-Domb ', () => {
         expect(result.toString()).toEqual(expected.toString())
     })
 
-    it('mp_full_multiply', () => {
-        const num_words = 16
-        const word_size = 16
-        const a = BigInt('0x1111111111111111111111111111111111111111111111111111111111111111')
-        const a_words = to_words_le(a, num_words, word_size)
+    it('mp_full_multiply with random inputs', () => {
+        const num_runs = 100
+        const p = BigInt('0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001')
+        const n = p.toString(2).length
 
-        const b = BigInt('0x2222222222222222222222222222222222222222222222222222222222222222')
-        const b_words = to_words_le(b, num_words, word_size)
-
-        const result = mp_full_multiply(a_words, b_words, num_words, word_size)
-
-        const expected = to_words_le(a * b, num_words * 2, word_size)
-
-        expect(result.toString()).toEqual(expected.toString())
+        for (let word_size = 13; word_size < 17; word_size ++) {
+            for (let i = 0; i < num_runs; i ++) {
+                const num_words = Math.ceil(n / word_size)
+                const a = genRandomFieldElement(p)
+                const b = genRandomFieldElement(p)
+                const a_words = to_words_le(a, num_words, word_size)
+                const b_words = to_words_le(b, num_words, word_size)
+                const result = mp_full_multiply(a_words, b_words, num_words, word_size)
+                const expected = to_words_le(a * b, num_words * 2, word_size)
+                expect(result.toString()).toEqual(expected.toString())
+            }
+        }
     })
 
-    it('machine_multiply', () => {
-        //const a = 0xff
-        //const b = 0xfab
-        const a = 44273
-        const b = 53641
-        const res = machine_multiply(a, b, 16)
-        expect(res.toString()).toEqual([19961, 36237].toString())
+    it('machine_multiply with random inputs', () => {
+        const num_runs = 100
+        for (let word_size = 13; word_size < 17; word_size ++) {
+            for (let i = 0; i < num_runs; i ++) {
+                const a = Number(genRandomFieldElement(BigInt(2 ** word_size)))
+                const b = Number(genRandomFieldElement(BigInt(2 ** word_size)))
+                const res = machine_multiply(a, b, 16)
+                const expected = to_words_le(BigInt(a * b), 2, 16)
+                expect(res.toString()).toEqual(expected.toString())
+            }
+        }
     })
 
-    it('machine_two_digit_add', () => {
-        const a = [25512, 7628]
-        const b = [55494, 14279]
-        const expected = [15470, 21908, 0]
+    it('machine_two_digit_add with random inputs', () => {
+        const num_runs = 100
+        for (let word_size = 13; word_size < 17; word_size ++) {
+            for (let i = 0; i < num_runs; i ++) {
+                const a = genRandomFieldElement(BigInt(2) ** BigInt(2 * word_size))
+                const a_words = Array.from(to_words_le(a, 2, word_size))
+                const b = genRandomFieldElement(BigInt(2) ** BigInt(2 * word_size))
+                const b_words = Array.from(to_words_le(b, 2, word_size))
 
-        //const a = [0xfff0, 0xfffe]
-        //const b = [0xfff1, 0xfff2]
-        const res = machine_two_digit_add(a, b, 16)
-        expect(res.toString()).toEqual(expected.toString())
+                const expected = to_words_le(a + b, 3, word_size)
+                const res = machine_two_digit_add(a_words, b_words, word_size)
+                expect(res.toString()).toEqual(expected.toString())
+            }
+        }
     })
 
     it('barrett_domb_mul', () => {
