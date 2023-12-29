39e1bd898b43c6212bfc42c3ce066bf40622c865
Koh Wei Jie <contact@kohweijie.com>
2023-12-29 21:15:40 +0800

    small cuzk impl in typescript

--- src/submission/cuzk/cuzk.test.ts
+++ src/submission/cuzk/cuzk.test.ts
@@ -1,81 +1,94 @@
-// import { assert } from "console"
-// import { BigIntPoint, U32ArrayPoint } from "../../reference/types";
-// import { bigIntToU32Array, generateRandomFields } from '../../reference/webgpu/utils';
-// import { init, transpose_and_spmv, smtvp } from './cuzk_serial'
-// import { fieldMath } from '../matrices/matrices'; 
+jest.setTimeout(10000000)
+import { FieldMath } from "../../reference/utils/FieldMath";
+import { ExtPointType } from "@noble/curves/abstract/edwards";
+import { decompose_scalars } from '../../submission/utils'
+import { cpu_transpose } from '../../submission/cuzk/transpose'
+import { cpu_smvp } from '../../submission/cuzk/smvp';
 
-// export const generate_points = async(inputSize: number): Promise<{
-//     bigIntPoints: BigIntPoint[],
-//     U32Points: U32ArrayPoint[],
-//   }> => {
-//     // Creating random points is slow, so for now use a single fixed base.
-//     const x = BigInt('2796670805570508460920584878396618987767121022598342527208237783066948667246');
-//     const y = BigInt('8134280397689638111748378379571739274369602049665521098046934931245960532166');
-//     const t = BigInt('3446088593515175914550487355059397868296219355049460558182099906777968652023');
-//     const z = BigInt('1');
-//     const point: BigIntPoint = {x, y, t, z};
-//     const bigIntPoints: BigIntPoint[] = Array(inputSize).fill(point);
-//     const U32Points: U32ArrayPoint[] = bigIntPoints.map((point) => {
-//         return {
-//         x: bigIntToU32Array(point.x),
-//         y: bigIntToU32Array(point.y),
-//         t: bigIntToU32Array(point.t),
-//         z: bigIntToU32Array(point.z),
-//         }});
+const fieldMath = new FieldMath()
+const x = BigInt('2796670805570508460920584878396618987767121022598342527208237783066948667246')
+const y = BigInt('8134280397689638111748378379571739274369602049665521098046934931245960532166')
+const t = BigInt('3446088593515175914550487355059397868296219355049460558182099906777968652023')
+const z = BigInt('1')
+const pt = fieldMath.createPoint(x, y, t, z)
 
-//     return { bigIntPoints, U32Points }
-// }
+describe('cuzk', () => {
+    it('one subtask', () => {
+        const input_size = 32
+        const chunk_size = 4
 
-// export const generate_scalars = async(inputSize: number): Promise<{
-//     bigIntScalars: bigint[],
-//     U32Scalars: Uint32Array[]
-//   }> => {
-//     const bigIntScalars: bigint[] = generateRandomFields(inputSize);
-//     const U32Scalars: Uint32Array[] = bigIntScalars.map((scalar) => bigIntToU32Array(scalar));
-//     return { bigIntScalars, U32Scalars }
-// }
+        const num_columns = 2 ** chunk_size
+        const num_rows_per_subtask = input_size
 
-// describe('cuzk serial test', () => {
-//     describe('', () => {
-//         it('spmv_and_sptvm_test', async () => {
-//             let inputSize = 16
-//             let points = await generate_points(inputSize)
-//             let scalars = await generate_scalars(inputSize)
-            
-//             // Define sample scalars
-//             scalars.bigIntScalars[0] = BigInt(1155)
-//             scalars.bigIntScalars[1] = BigInt(9206)
-//             scalars.bigIntScalars[2] = BigInt(2050)
-//             scalars.bigIntScalars[3] = BigInt(8173)
-//             scalars.bigIntScalars[4] = BigInt(38313)
-//             scalars.bigIntScalars[5] = BigInt(28598)
-//             scalars.bigIntScalars[6] = BigInt(54472)
-//             scalars.bigIntScalars[7] = BigInt(61523)
-//             scalars.bigIntScalars[8] = BigInt(3823)
-//             scalars.bigIntScalars[9] = BigInt(29232)
-//             scalars.bigIntScalars[10] = BigInt(2934)
-//             scalars.bigIntScalars[11] = BigInt(10239)
-//             scalars.bigIntScalars[12] = BigInt(8374)
-//             scalars.bigIntScalars[13] = BigInt(10384)
-//             scalars.bigIntScalars[14] = BigInt(53621)
-//             scalars.bigIntScalars[15] = BigInt(8372)
+        const num_chunks_per_scalar = Math.ceil(256 / chunk_size)
+        const num_subtasks = num_chunks_per_scalar
 
-//             // Initialize instance 
-//             const csr_sparse_matrix_array = await init(inputSize, points.bigIntPoints, scalars.bigIntScalars)
+        const points: ExtPointType[] = []
+        const scalars: bigint[] = []
+        for (let i = 0; i < input_size; i ++) {
+            const p = BigInt('0x12ab655e9a2ca55660b44d1e5c37b00159aa76fed00000010a11800000000001')
+            const v = BigInt('0x1a1b13111111112c11b8111113331111711111f1111111111111311131131121111811171111')
+            scalars.push((BigInt(i) * v) % p)
+            points.push(fieldMath.createPoint(x, y, t, z))
+        }
 
-//             // Perform Transpose and SPMV 
-//             const cuzk_result_1 = await transpose_and_spmv(csr_sparse_matrix_array[15])
-//             console.log(cuzk_result_1)
-            
-//             // Perform SMTVP
-//             const cuzk_result_2 = await smtvp(csr_sparse_matrix_array[15])
-//             console.log(cuzk_result_2)
+        const decomposed_scalars = decompose_scalars(scalars, num_chunks_per_scalar, chunk_size)
 
-//             // Assertion checks
-//             assert(cuzk_result_1.x === cuzk_result_2.x)
-//             assert(cuzk_result_1.y === cuzk_result_2.y)
-//         })
-//     })
-// })
+        const bucket_sums: ExtPointType[] = []
+        for (let subtask_idx = 0; subtask_idx < num_subtasks; subtask_idx ++) {
+            const subtask_chunks = decomposed_scalars[subtask_idx]
+
+            // Copy subtask_chunks to col_idx
+            const col_idx = subtask_chunks.map((x) => Number(x))
+
+            // Construct row_ptr
+            let j = 0
+            const row_ptr: number[] = [0]
+            for (let i = 0; i < subtask_chunks.length; i += num_columns) {
+                row_ptr.push(row_ptr[j] + num_columns)
+                j ++
+            }
+
+            const {
+                csc_col_ptr,
+                csc_row_idx,
+                csc_vals,
+            }  = cpu_transpose(row_ptr, col_idx, num_columns)
+
+            // SMVP
+            const buckets = cpu_smvp(csc_col_ptr, points, fieldMath)
+
+            let bucket_sum = fieldMath.customEdwards.ExtendedPoint.ZERO
+            for (let i = 1; i < buckets.length; i ++) {
+                const b = buckets[i].multiply(BigInt(i))
+                bucket_sum = bucket_sum.add(b)
+            }
+            bucket_sums.push(bucket_sum)
+        }
+
+        // Horner's rule
+        const m = BigInt(2) ** BigInt(chunk_size)
+        // The last scalar chunk is the most significant digit (base m)
+        let result = bucket_sums[bucket_sums.length - 1]
+        for (let i = bucket_sums.length - 2; i >= 0; i --) {
+            result = result.multiply(m)
+            result = result.add(bucket_sums[i])
+        }
+
+        const result_affine = result.toAffine()
+        console.log(result_affine)
+
+        // Calculated expected result
+        let expected = fieldMath.customEdwards.ExtendedPoint.ZERO
+        for (let i = 0; i < input_size; i ++) {
+            if (scalars[i] !== BigInt(0)) {
+                const p = points[i].multiply(scalars[i])
+                expected = expected.add(p)
+            }
+        }
+        const expected_affine = expected.toAffine()
+        console.log(expected_affine)
+    })
+})
 
 export {}
