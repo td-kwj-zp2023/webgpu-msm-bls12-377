02f37317451f1a26822ab70e75d4ab87540ffcac
Koh Wei Jie <contact@kohweijie.com>
2024-01-02 09:36:49 +0800

    mock cuzk with signed indices done

--- src/submission/cuzk/smvp.ts
+++ src/submission/cuzk/smvp.ts
@@ -1,28 +1,50 @@
 import { FieldMath } from "../../reference/utils/FieldMath";
 import { ExtPointType } from "@noble/curves/abstract/edwards";
 
+// Perform SMVP and scalar mul with signed bucket indices.
 export const cpu_smvp = (
     csc_col_ptr: number[],
     csc_val_idxs: number[],
     points: ExtPointType[],
+    chunk_size: number,
     fieldMath: FieldMath,
 ) => {
+    const l = 2 ** chunk_size
+    const h = l / 2
+    const num_columns = csc_col_ptr.length - 1
     const currentSum = fieldMath.customEdwards.ExtendedPoint.ZERO;
 
-    const result: ExtPointType[] = [];
+    const buckets: ExtPointType[] = [];
     for (let i = 0; i < csc_col_ptr.length - 1; i++) {
-        result.push(currentSum)
+        buckets.push(currentSum)
     }
 
-    for (let i = 0; i < csc_col_ptr.length - 1; i++) {
-        const row_begin = csc_col_ptr[i];
-        const row_end = csc_col_ptr[i + 1];
-        let sum = currentSum;
-        for (let j = row_begin; j < row_end; j++) {
-            sum = sum.add(points[csc_val_idxs[j]])
+    // In a GPU implementation, each iteration of this loop should be performed by a thread.
+    // Each thread handles two buckets:
+    // i - h and
+    // i + h - l / 2
+    for (let idx = 0; idx < num_columns / 2; idx ++) {
+        for (let j = 0; j < 2; j ++) {
+            const i = idx * 2 + j
+            const row_begin = csc_col_ptr[i];
+            const row_end = csc_col_ptr[i + 1];
+            let sum = currentSum;
+            for (let j = row_begin; j < row_end; j++) {
+                sum = sum.add(points[csc_val_idxs[j]])
+            }
+
+            const x = i - l / 2
+
+            let bucket_idx = x
+            if (x < 0) {
+                bucket_idx = x * -1
+                sum = sum.negate()
+            }
+
+            if (bucket_idx > 0) {
+                buckets[bucket_idx] = buckets[bucket_idx].add(sum.multiply(BigInt(bucket_idx)))
+            }
         }
-        result[i] = sum
     }
-
-    return result
+    return buckets
 }
