e06c67caf51083837fcca053ce6ef6d259444798
Koh Wei Jie <contact@kohweijie.com>
2024-01-08 23:38:44 +0800

    removed CSR web workers

--- src/submission/submission.ts
+++ src/submission/submission.ts
@@ -1,6 +1,5 @@
 import { BigIntPoint } from "../reference/types";
 import { execute_cuzk } from "./cuzk/cuzk_serial"
-import { execute_cuzk_parallel } from "./cuzk/cuzk_parallel"
 import { transpose } from "./cuzk/transpose_wgsl"
 import { smtvp } from "./cuzk/smtvp_wgsl"
 import { smvp } from "./cuzk/smvp_wgsl"
@@ -13,22 +12,6 @@ export const cuzk_typescript_serial = async (
   console.log("Starting Serial cuZK!")
 
   const result = await execute_cuzk(baseAffinePoints, scalars)
-
-  const result_affine = result.toAffine()
-  const x = result_affine.x
-  const y = result_affine.y
-
-  return { x, y }
-};
-
-// Typescript implementation of cuZK with web-workers
-export const cuzk_typescript_web_workers = async (
-  baseAffinePoints: BigIntPoint[],
-  scalars: bigint[]
-): Promise<{x: bigint, y: bigint}> => {
-  console.log("Starting Parallel cuZK!")
-
-  const result = await execute_cuzk_parallel(baseAffinePoints, scalars)
 
   const result_affine = result.toAffine()
   const x = result_affine.x
